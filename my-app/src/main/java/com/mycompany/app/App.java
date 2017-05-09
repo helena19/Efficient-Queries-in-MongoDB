bc0d39eb9a99d8a37fe03f58e13e5fc5dfef0d5b
helena_19 <sdi1300091@di.uoa.gr>
2017-05-09 20:07:51 +0300

    1st try

---
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,23 @@
+package com.mycompany.app;
+
+import java.net.UnknownHostException;
+import java.util.Date;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoException;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        MongoClient mongo = new MongoClient( "localhost" , 27017 );
+    }
+}
