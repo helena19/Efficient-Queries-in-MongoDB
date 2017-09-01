d29943a3432e6d8b9ce6150b4203171e536f6bdd
ariannatr <sdi1300179@di.uoa.gr>
2017-09-01 19:42:11 +0300

    setEncryptedField ,getEncryptedFields implemented

--- my-app/src/main/java/gr/uoa/di/ae/thesis/AEMongoCollection.java
+++ my-app/src/main/java/gr/uoa/di/ae/thesis/AEMongoCollection.java
@@ -1,12 +1,17 @@
 package gr.uoa.di.ae.thesis;
 
-//import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Scanner;
 
 import org.bson.Document;
 //import org.bson.codecs.configuration.CodecRegistry;
 //import org.bson.conversions.Bson;
 
+import com.mongodb.BasicDBObject;
 /*import com.mongodb.ReadConcern;
 import com.mongodb.ReadPreference;
 import com.mongodb.WriteConcern;
@@ -19,8 +24,11 @@ import com.mongodb.client.model.CreateViewOptions;*/
 
 public class AEMongoCollection {
 	
+	private Map<String,EncryptionType> encrypted_fields;
+	
 	public AEMongoCollection(MongoCollection<Document> collection) {
 		// TODO Auto-generated constructor stub
+		encrypted_fields=new HashMap<String,EncryptionType>();
 	}
 
 	public void AEMongoCollectionCreateDoc(MongoCollection<Document> collection) {	
@@ -47,5 +55,48 @@ public class AEMongoCollection {
 		} 
 		keyboard.close();
 	}
+	
+	public void setEncryptedField(String field,EncryptionType enc)
+	{
+		if(encrypted_fields.containsKey(field))
+			System.out.println("You have already defined an encryption type for the field "+field);
+		else
+			encrypted_fields.put(field, enc);
+	}
+	
+	public List<String> getEncryptedFields()
+	{
+		List<String> enc_fields;
+		enc_fields= new ArrayList<String>() ;
+		for(String field: encrypted_fields.keySet())
+		{
+			enc_fields.add(field);
+		}
+		return enc_fields;
+	}
+	
+	public void insertOne(Document document,MongoCollection<Document> collection) 
+	{
+		for(Entry<String, Object> field:document.entrySet())
+		{
+			String field_name=field.getKey();
+			Object field_value=field.getValue();
+			if(encrypted_fields.containsKey(field_name))
+			{
+				EncryptionType enc=encrypted_fields.get(field_name);
+				System.out.println("I have to encrypt field "+field_name+" the value "+field_value);
+				field.setValue("encrypted-email:)");
+			}
+					
+		}
+		collection.insertOne(document);
+	}
+	
+	public Document find(Document document)
+	{
+		//TO-DO
+		return null;
+	}
+	
 	
 }
--- my-app/src/main/java/gr/uoa/di/ae/thesis/App.java
+++ my-app/src/main/java/gr/uoa/di/ae/thesis/App.java
@@ -4,27 +4,49 @@ import java.util.Arrays;
 
 import org.bson.Document;
 
+import com.mongodb.BasicDBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoCredential;
 import com.mongodb.ServerAddress;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
 
 public class App
 {	
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-//        MongoClient mongo = new MongoClient( "ds143151.mlab.com" , 43151 );
         
         MongoCredential credential = MongoCredential.createCredential("admin", "thesisdi", "asdf-1234!".toCharArray());
         MongoClient mongoClient = new MongoClient(new ServerAddress("ds143151.mlab.com:43151"), Arrays.asList(credential));
+
         MongoDatabase db = mongoClient.getDatabase("thesisdi");
         MongoCollection<Document> collection = db.getCollection("thesis");
-        System.out.println(collection.count());
+        System.out.println("The collection has "+collection.count()+" items");
         AEMongoCollection myCollection = new AEMongoCollection(collection);
-        myCollection.AEMongoCollectionCreateDoc(collection);
-        System.out.println(collection.count());
+        
+        /*Set the encrypted field*/
+        myCollection.setEncryptedField("e-mail",EncryptionType.HASH);
+        
+        /*New Entry*/
+		Document document = new Document("name", "Michael").append("e-mail", "mike@bulls.com");
+        myCollection.insertOne(document,collection);
+        
+        //myCollection.AEMongoCollectionCreateDoc(collection);
+        System.out.println("Now,The collection has "+collection.count()+" items");
+        
+        System.out.println("The encrypted fields so far "+myCollection.getEncryptedFields());
+        
+        /*Print All Inserts At the Collection*/
+        MongoCursor<Document> cursor = collection.find().iterator();
+        try {
+            while (cursor.hasNext()) {
+                System.out.println(cursor.next().toJson());
+            }
+        } finally {
+            cursor.close();
+        }
+        
         
 //        BasicDBObject doc = new BasicDBObject("name", "Mary").append("surname", "Jane").append("age", 21).append("class", 2014);
 //        BasicDBObject doc = new BasicDBObject("name", "MongoDB").append("type", "database").append("count", 1).append("info", new BasicDBObject("x", 203).append("y", 102));
