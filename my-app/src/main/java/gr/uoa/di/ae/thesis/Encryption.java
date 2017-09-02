e3463f2d8837c1e977dc3dafd3e534659a641cef
ariannatr <sdi1300179@di.uoa.gr>
2017-09-02 03:07:34 +0300

    enc,dec class

---
+++ my-app/src/main/java/gr/uoa/di/ae/thesis/Encryption.java
@@ -0,0 +1,43 @@
+package gr.uoa.di.ae.thesis;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+public class Encryption {
+		
+	public String sha256_encrypt(String str)
+	{
+		MessageDigest digest = null;
+		try {
+			digest = MessageDigest.getInstance("SHA-256");
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		byte[] hash = digest.digest(str.getBytes(StandardCharsets.UTF_8));
+		String encoded = Base64.getEncoder().encodeToString(hash);
+	    return encoded;
+	}
+	
+	public String sha256_decrypt(String str)
+	{
+		MessageDigest digest = null;
+		try {
+			digest = MessageDigest.getInstance("SHA-256");
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		byte[] hash = digest.digest(str.getBytes(StandardCharsets.UTF_8));
+		byte [] decoded_bytes = Base64.getDecoder().decode(hash);
+//		String decoded=new String(decoded_bytes); 
+//		System.out.println("The decoded String "+decoded);
+//		byte[] decodedBytes = Base64.getDecoder().decode(str.getBytes());
+		String decoded=new String(decoded_bytes,Charset.forName("UTF-8")); 
+		System.out.println("The decoded String "+decoded);
+	    return decoded;
+	}
+}
